4a60983fde534a87d73e06399ebb93eabb80e162
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2021-06-08 12:36:54 -0400

    did more random shit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function ohfuck(amount)
 {
+	console.log(typeof(document.getElementById("ohfuck").style.fontSize));
+	currentSize = parseFloat(document.getElementById("ohfuck").style.fontSize);
+	console.log(currentSize);
+	currentSize += 5;
 	for(i = 0; i < amount; i++){
-		document.getElementById("ohfuck").innerHTML += "\n oh fuck";
+		document.getElementById("ohfuck").innerHTML += " oh fuck";
 	}
+	document.getElementById("ohfuck").style.fontSize = currentSize + "px";
 }
 ohfuck(10);
